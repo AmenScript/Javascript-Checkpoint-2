233cba46aa4b875d2127dc80afef2801145f83fa
Amen <amenal@tuta.io>
2024-07-30 09:32:41 +0100

    Update Javascript Checkpoint 2.js

--- Javascript Checkpoint 2.js
+++ Javascript Checkpoint 2.js
@@ -74,3 +74,11 @@ function checkPrime(n) {
       return true;  
     }
   }
+
+function fibonacci(num) {
+    if (num == 1)
+        return 0;
+    if (num == 2)
+        return 1;
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
